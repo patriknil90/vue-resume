5a79bf9a71b13822da5759310dfdc85b7a62f34e
Patrik Nilsson <patriknil90@gmail.com>
2019-03-30 19:39:57 +0100

    Set up boilerplate from vue-webpack-babel-starter repo

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="app-container">
+    <h1>{{ message }}</h1>
+    <img src="./images/vue_logo.png">
+  </div> 
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: 'Hello, Vue!'
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+  .app-container { 
+    text-align: center;
+    h1 { font-weight: 100; }
+    img { max-width: 90%; }
+  }
+</style>
+
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const { VueLoaderPlugin } = require('vue-loader');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                exclude: /node_modules/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(gif|svg|jpg|png)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'images',
+                            name: '[name].[ext]'
+                        },
+                    },
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({
+          template: './src/index.html'
+        })
+    ]
+};
