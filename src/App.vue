3df05a732e3ac2eca76e61f68c95d0bf7f30cb97
Patrik <patriknil90@gmail.com>
2019-04-04 23:19:48 +0200

    New layout, lorem presentation and buttons

--- src/App.vue
+++ src/App.vue
@@ -9,35 +9,59 @@
           >
             <v-flex
               xs12
-              lg4
+              md4
+              lg3
             >
-              <div class="resume-column grey darken-4 white--text">
-                <div class="info-column-section">
-                  <img
-                    class="portrait mt-5 mb-4 elevation-5"
-                    src="./images/patrik-nilsson.jpg"
-                    alt="Patrik Nilsson portrait"
-                  >
-                  <div class="presentation-titles text-xs-center">
-                    <h1 class="name-title font-weight-thin">
-                      Patrik Nilsson
-                    </h1>
-                    <h2 class="work-title font-weight-light">
-                      Front End Developer
-                    </h2>
-                  </div>
-                </div>
+              <div class="resume-column">
+                <img
+                  class="portrait"
+                  src="./images/patrik-nilsson.jpg"
+                  alt="Patrik Nilsson portrait"
+                >
               </div>
             </v-flex>
             <v-flex
               xs12
-              lg8
+              md8
+              lg9
             >
               <div class="resume-column">
                 <div class="main-section">  
-                  <h2 class="main-section-title">
-                    Profile
-                  </h2>
+                  <div class="presentation-titles">
+                    <h1 class="name-title font-weight-thin">
+                      Patrik Nilsson
+                    </h1>
+                    <h2 class="work-title font-weight-light mb-4">
+                      Front End Developer
+                    </h2>
+                    <p class="personal-info grey--text text--darken-1 font-weight-light">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas temporibus reprehenderit dignissimos laborum quos nisi hic dolorum, illum officia. Expedita voluptate ut quidem unde incidunt voluptatibus, at enim quas nisi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique dicta quasi, quisquam totam, iusto dolorem quod, voluptatibus a minus eligendi quae culpa deserunt itaque maxime. Unde quas in minima repellendus.
+                    </p>
+                    <v-btn
+                      depressed
+                      class="blue darken-1 white--text"
+                      href="https://www.linkedin.com/in/patriknil90/"
+                      target="__blank"
+                    >
+                      Linkedin
+                    </v-btn>
+                    <v-btn
+                      depressed
+                      class="blue-grey darken-4 white--text"
+                      href="https://github.com/patriknil90"
+                      target="__blank"
+                    >
+                      Github
+                    </v-btn>
+                    <v-btn
+                      depressed
+                      class="indigo white--text"
+                      href="https://www.facebook.com/patriknil90"
+                      target="__blank"
+                    >
+                      Facebook
+                    </v-btn>
+                  </div>
                 </div>
               </div>
             </v-flex>
@@ -69,6 +93,8 @@ export default {
     }
   }
 
+  .main-section { padding: 10px 30px; }
+
   .resume-paper { background-color: white; }
 
   .info-column-section {
@@ -77,14 +103,19 @@ export default {
   }
   .portrait { 
     display: block;
-    width: 175px;
-    margin: auto;
-    border-radius: 3px;
+    width: 100%;
   }
 
   .name-title {
     font-family: 'Slabo 27px', serif;
-    font-size: 40px;
+    font-size: 50px;
+    line-height: 1.4;
   }
+
+  .personal-info {
+    font-size: 15px;
+    line-height: 1.8;
+  }
+
 </style>
 
