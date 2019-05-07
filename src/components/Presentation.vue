f5b3f37cb9820987b02f1e4a311732a190cb745e
Patrik <patriknil90@gmail.com>
2019-05-07 16:51:34 +0200

    Add site purpose

--- src/components/Presentation.vue
+++ src/components/Presentation.vue
@@ -1,26 +1,42 @@
 <template>
-  <p class="personal-info grey--text text--darken-1 font-weight-light">
-    {{ presentation }}
-  </p>
+  <div class="personal-info grey--text text--darken-1 font-weight-light">
+    <p
+      v-for="presentationType in presentation"
+      :key="presentationType"
+      v-html="presentationType"
+    />
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      presentation: `I'm a structured and result-oriented Front End developer specializing in Single Page applications.
+      presentation: {
+        sitePurpose: `<span style="font-style: italic;">This site was created during a spare time project as a way for me to familiarize myself a bit 
+          more with Vue.js, WebPack, Babel ans Amazon Web Services.
+          As a bonus, it will also live as my personal digital resume. The source code for this site can be found 
+          <a href="https://github.com/patriknil90/vue-resume" target="_blank">here</a>.</span>`,
+        work: `I'm a structured and result-oriented Front End developer specializing in Single Page applications.
           With an agile approach and focus on problem solving I want to contribute value to end-users and businesses without being locked in to one SPA framework.
           In addition to my experience building Single Page Applications, I've developed web applications and websites professionally since 2014.
-          I've had the oppurtunity to experience the pros and cons of working in both small startups and global organizations.`
+          I've had the oppurtunity to experience the pros and cons of working in both small startups and global organizations.` 
+      }
     }
   }
 }
 </script>
 
-<style>
+<style lang="scss">  
   .personal-info {
     font-size: 15px;
     line-height: 1.8;
+    a {
+      text-decoration: none;
+      &:hover {
+        text-decoration: underline;
+      }
+    }
   }
 </style>
 
