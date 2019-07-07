339a88d024781a62a799f78da23c7456099ae855
Patrik <patriknil90@gmail.com>
2019-07-07 11:13:48 +0200

    Add content to About me section

--- src/components/resume/Presentation.vue
+++ src/components/resume/Presentation.vue
@@ -4,7 +4,10 @@
       About me
     </h2>
     <p class="milestone-section section-presentation">
-      How fun that you've found my site and want to read about me! If I would describe myself I would say that I'm structured, organized, forward-looking and goal-oriented. I guess you could say that I always want to see a clear path ahead and that I prefer strategies over ad hoc. I have an agile approach to life which means that I believe in incrementally developing myself, processes, teams or other things to better achieve great things. I strongly believe in doing what I love to contribute as much as I can. In my career, I love to develop business applications as a Front End developer.<br><br>In my spare time I like to exercise, go to seminars and meetups, listen to podcasts and have as many extraordinary experiences as I can (be that of experiencing a new part of the world, trying out new food or meeting new exciting people).
+      How fun that you've found my site and want to read about me! If I would describe myself I would say that I'm structured, organized, forward-looking and goal-oriented. I guess you could say that I always want to see a clear path ahead and that I prefer strategies over ad hoc. I have an agile approach to life which means that I believe in incrementally developing myself, processes, teams or other things to better achieve greatness. I strongly believe in doing what I love to contribute as much as I can. In my career, I love to develop business applications as a Front End developer.<br><br>In my spare time I like to exercise, go to seminars and meetups, listen to podcasts and have as many extraordinary experiences as I can, be that of experiencing a new part of the world, trying out new food or meeting new exciting people.<br><br>I'm always open to new relationships. If you would like to connect with me, don't hesitate to add me on <a
+        href="https://www.linkedin.com/in/patriknil90/"
+        target="_blank"
+      >LinkedIn</a> or send me an <a href="mailto:patriknil90@gmail.com">email</a>.<br><br>BR, Patrik
     </p>
   </div>
 </template>
